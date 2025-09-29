5f6bf6b348ef75d2443a65e5a9ab2804ad262992
Raihaninkam <raihankamil37@gmail.com>
2025-09-29 15:39:35 +0700

    fix(func): fix main.go

--- main.go
+++ main.go
@@ -2,20 +2,19 @@ package main
 
 func StringShortener(sentence string) []string {
 
-	result := []string{}
+	result := []rune{}
 
 	for _, v := range sentence {
 		// jika tidak kosong dan huruf terakhir = huruf sekarang
-		if len(result) > 0 && result[len(result)-1] == string(v) {
+		if len(result) > 0 && result[len(result)-1] == v {
 			// hapus yang terakhir
 			result = result[:len(result)-1]
 		} else {
 			// tambah huruf ke result
-			result = append(result, string(v))
-			return result
+			result = append(result, v)
+
 		}
 
 	}
-	return []string{}
-
+	return []string{string(result)}
 }
